aead5de6de5bd4f8c448deaa36351537787e7a5f
v7nny <fugoldima05@gmail.com>
2026-01-24 15:28:31 +0300

    Init commit

---
+++ src/test/java/v7nny/bank/card/ApplicationTests.java
@@ -0,0 +1,13 @@
+package v7nny.bank.card;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
